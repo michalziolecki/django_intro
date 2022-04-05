e4ffc8773072687aa5b18f12dd47cee1d91677b2
Michal Ziolecki <mziolecki@blackberry.com>
2022-04-05 23:12:13 +0200

    next FBV

--- books/views.py
+++ books/views.py
@@ -1,7 +1,77 @@
-from django.http import HttpResponse
+import json
+from uuid import uuid4
+
+from django.core.exceptions import BadRequest
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
-def hello_world(request) -> HttpResponse:
+def get_hello_world(request: WSGIRequest) -> HttpResponse:
     return HttpResponse("Hello world")
 
+
+def get_uuids_list_a(request: WSGIRequest) -> HttpResponse:
+    uuids = [str(uuid4()) for _ in range(10)]
+    uuids_as_json = json.dumps(uuids)
+    return HttpResponse(uuids_as_json)
+
+
+def get_uuids_list_b(request: WSGIRequest) -> JsonResponse:
+    uuids = [str(uuid4()) for _ in range(10)]
+    return JsonResponse({"uuids": uuids})
+
+
+def get_argument_from_path(request: WSGIRequest, first_arg: int, second_arg: str, third_arg: str) -> HttpResponse:
+    """
+    ex. 13
+    """
+    return HttpResponse(f"first_arg={first_arg}, second_arg={second_arg}, third_arg={third_arg}")
+
+
+def get_arguments_from_query(request: WSGIRequest) -> HttpResponse:
+    a = request.GET.get("a")
+    b = request.GET.get("b")
+    c = request.GET.get("c")
+    print(type(a))  # str - type casting needed
+    print(type(b))  # str
+    print(type(c))  # str
+    return HttpResponse(f"a={a}, b={b}, c={c}")
+
+
+@csrf_exempt  # here with that decorator we turn off CSRF in that function
+def check_http_query_type(request: WSGIRequest) -> HttpResponse:
+    """
+    Remember to turn off CSRF token, ONLY FOR TESTS - for a while
+    """
+    query_type = "?"
+    if request.method == "GET":
+        query_type = "to jest GET - tutaj nie powinnismy dodawac i czytac BODY"
+    elif request.method == "POST":
+        query_type = "to jest POST - tutaj dane leca w BODY"
+    elif request.method == "PUT":
+        query_type = "to jest PUT - tutaj dane leca w BODY"
+    elif request.method == "DELETE":
+        query_type = "to jest DELETE - tutaj nie powinnismy dodawac i czytac BODY"
+    """
+    haslo ciekawostka - model dojrzalosci Richardsona 
+    https://devkr.pl/2018/04/10/restful-api-richardson-maturity-model/
+    """
+    return HttpResponse(query_type)
+
+
+def get_headers(request: WSGIRequest) -> JsonResponse:
+    """
+    HTTP - text protocol
+            HEADERS - some data as dict
+            \n\n
+            BODY - some data as dict
+    """
+    headers = request.headers
+    return JsonResponse({"headers": dict(headers)})
+
+
+def raise_error_for_fun(request: WSGIRequest):
+    raise BadRequest("My error")
+    # return HttpResponse()
--- library/urls.py
+++ library/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import hello_world
+from books.views import get_hello_world, get_uuids_list_a, get_uuids_list_b, get_argument_from_path, \
+    get_arguments_from_query, check_http_query_type, get_headers, raise_error_for_fun
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_world, name="home")
+    path('', get_hello_world, name="home"),
+    path('uuids_a', get_uuids_list_a, name="uuids_a"),
+    path('uuids_b', get_uuids_list_b, name="uuids_b"),
+    path('path-args/<int:first_arg>/<str:second_arg>/<slug:third_arg>', get_argument_from_path, name="path_args"),
+    path('query-args', get_arguments_from_query, name="query_args"),
+    path('check-http-type', check_http_query_type, name="check_http_type"),
+    path('get-headers', get_headers, name="get_headers"),
+    path('raise-error', raise_error_for_fun, name="raise_error"),
 ]
