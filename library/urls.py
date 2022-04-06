eddd596ab097fdfe1de8f2ac4ec5674f5b99869b
Michal Ziolecki <mziolecki@blackberry.com>
2022-04-06 22:22:24 +0200

    urls moved to books

--- library/urls.py
+++ library/urls.py
@@ -14,19 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from books.views import get_hello_world, get_uuids_list_a, get_uuids_list_b, get_argument_from_path, \
-    get_arguments_from_query, check_http_query_type, get_headers, raise_error_for_fun
+# from books.views import get_hello_world, get_uuids_list_a, get_uuids_list_b, get_argument_from_path, \
+#     get_arguments_from_query, check_http_query_type, get_headers, raise_error_for_fun
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_hello_world, name="home"),
-    path('uuids_a', get_uuids_list_a, name="uuids_a"),
-    path('uuids_b', get_uuids_list_b, name="uuids_b"),
-    path('path-args/<int:first_arg>/<str:second_arg>/<slug:third_arg>', get_argument_from_path, name="path_args"),
-    path('query-args', get_arguments_from_query, name="query_args"),
-    path('check-http-type', check_http_query_type, name="check_http_type"),
-    path('get-headers', get_headers, name="get_headers"),
-    path('raise-error', raise_error_for_fun, name="raise_error"),
+    path('books/', include('books.urls'))
+    # path('', get_hello_world, name="home"),
+    # path('uuids_a/', get_uuids_list_a, name="uuids_a"),
+    # path('uuids_b/', get_uuids_list_b, name="uuids_b"),
+    # path('path-args/<int:first_arg>/<str:second_arg>/<slug:third_arg>/', get_argument_from_path, name="path_args"),
+    # path('query-args/', get_arguments_from_query, name="query_args"),
+    # path('check-http-type/', check_http_query_type, name="check_http_type"),
+    # path('get-headers/', get_headers, name="get_headers"),
+    # path('raise-error/', raise_error_for_fun, name="raise_error"),
 ]
