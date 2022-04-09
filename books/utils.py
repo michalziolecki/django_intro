898199cb8ee91b6a04775fb2abe43231fce8444e
Michal Ziolecki <mziolecki@blackberry.com>
2022-04-10 01:12:31 +0200

    corrections

--- books/utils.py
+++ books/utils.py
@@ -1,3 +1,4 @@
+import math
 from typing import List, Dict, Any
 
 import requests
@@ -12,26 +13,54 @@ google_logger = getLogger("GoogleLogger")
 
 
 def get_books_from_google_api(query_arg: str = "*") -> List[Book]:
+    """
+    usage is here https://developers.google.com/books/docs/v1/using
+        Pagination can be done by :
+        'For any request for all items in a collection, you can paginate
+        results by specifying startIndex and maxResults in the parameters for the request
+        Default: maxResults=10
+        Maximum allowable value: maxResults=40.'
+    """
     print("run")
-    books_volume_url = f"https://www.googleapis.com/books/v1/volumes?q={query_arg}&startIndex=0&maxResults=40"
+    books_volume_url = "https://www.googleapis.com/books/v1/volumes?q={query_arg}&startIndex={start}&maxResults={chunk_size}"
     books: List[Book] = []
     try:
-        google_response: Response = requests.get(books_volume_url)
+        google_response: Response = requests.get(books_volume_url.format(query_arg=query_arg, start=0, chunk_size=1))
     except RequestException as e:
         # google_logger.error(f"Encounter error while requesting to google, more: {e.args}")
         print(f"Encounter error while requesting to google, more: {e.args}")
         return []
     else:
         if 200 <= google_response.status_code < 300:
-            results: Dict[str, Any] = google_response.json()
-            # google_logger.info(results)
-            for item in results.get("items", []):
-                books.append(_build_book_from_item(item))
-            print(results)
+            total: int = google_response.json()["totalItems"]
+            for start_index in _generate_pagination(total):
+                try:
+                    print(start_index)
+                    response: Response = requests.get(books_volume_url.format(query_arg=query_arg, start=start_index, chunk_size=40))
+                except RequestException as e:
+                    # google_logger.error(f"Encounter error while requesting to google, more: {e.args}")
+                    print(f"Encounter error while requesting to google while pagination, more: {e.args}, start_index: {start_index}")
+                    continue
+                else:
+                    results = response.json()
+                    print(f"items.len={len(results.get('items', []))}")
+                    for item in results.get("items", []):
+                        try:
+                            books.append(_build_book_from_item(item))
+                        except:
+                            continue
         else:
             # google_logger.error(f"Request of {google_response.url} has status = {google_response.status_code}")
             print(f"Request of {google_response.url} has status = {google_response.status_code}")
         return books
+
+
+def _generate_pagination(total, start: int = 0, chunk_size: int = 40):
+    chunks_number = math.ceil(total/chunk_size)
+    for chunk_number in range(chunks_number):
+        if chunk_number != 0:
+            start = chunk_size * chunk_number
+        yield start
 
 
 def _build_book_from_item(item: Dict[str, Any]) -> Book:
