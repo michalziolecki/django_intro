9ee70ace80adf33163874ec3c3ceaf0b0dd42bf4
Michal Ziolecki <mziolecki@blackberry.com>
2022-05-15 12:10:19 +0200

    style with login and logout

--- users/models.py
+++ users/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, Permission
